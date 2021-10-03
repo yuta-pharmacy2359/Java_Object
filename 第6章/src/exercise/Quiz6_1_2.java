6fb4a31d2a11c96805249cb1b422a0e086a6b194
Yuta <yuta.shogai@gmail.com>
2021-10-03 20:49:37 +0900

    Javaオブジェクト 第6-1章終了

---
+++ 第6章/src/exercise/Quiz6_1_2.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class Quiz6_1_2 {
+	/*
+	  
+	 A ← B ← C
+	 A ← B ← D ← E 
+	 
+	 間違っているもの
+	 (1) CはAからの継承できるすべてのメンバを持っている → x
+	 (2) Aにはスーパークラスはない → ◯
+	 (3) BはtoString()メソッドを持っている → x
+	 (4) EはtoString()メソッドを持っている → x
+	  
+	 */
+}
